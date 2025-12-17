09b993230198336c1d034428006050f669a843be
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2025-12-17 20:09:30 +0530

    adding post in backend model

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('api.urls')),
+    path('post/', include('post.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
