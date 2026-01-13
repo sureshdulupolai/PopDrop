f9dc7d4a19113cd778d31d9a7767d0c2bb1172e9
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2026-01-13 20:56:31 +0530

    adding update on setting for email

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,12 +177,13 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sureshpolai6364@gmail.com'  # tumhara Gmail
-EMAIL_HOST_PASSWORD = 'ypfxylccvvnfjbpj'       # spaces hata ke
-DEFAULT_FROM_EMAIL = 'sureshpolai6364@gmail.com'
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
