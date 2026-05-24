43e3c8629648c182969303cd60d34ff328366b0b
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2026-05-24 14:02:36 +0530

    adding update on images part

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,11 @@ urlpatterns = [
     path('pop/', include('post.urls')),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.views.static import serve
+    from django.urls import re_path
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
